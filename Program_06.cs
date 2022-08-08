18257db628d5c2c4bbfffbf5a22d045fd0b703d5
Abhay_Kacha <100334404+ABHAYKACHA@users.noreply.github.com>
2022-08-08 14:47:53 +0530

    Add files via upload

---
+++ Program_06.cs
@@ -0,0 +1,20 @@
+﻿/*Write a C# Sharp program to print the output of multiplication of three numbers which will be
+entered by the user.
+Test Data:
+Input the first number to multiply: 2
+Input the second number to multiply: 3
+Input the third number to multiply: 6*/
+using System;
+
+class Program_06
+{
+    static void Main()
+    {
+        int a, b;
+        Console.WriteLine("enter the number 1 : ");
+        a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("enter the number 2 : ");
+        b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("the multiplication of 3 digits is : {0}",(a*b));
+    }
+}
